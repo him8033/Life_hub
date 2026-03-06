4f31bbdf1e88947841fa0f1cb7bb36f1c1e61a7a
Himanshu Gupta <himanshu8033@gmail.com>
2026-03-06 13:55:08 +0530

    update the missing category fetch url

--- travelhub/urls/spot_category.py
+++ travelhub/urls/spot_category.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from travelhub.views.category_views import (
     SpotCategoryListAPIView,
+    SpotCategoryDetailAPIView,
     SpotCategoryListCreateAPIView,
     SpotCategoryUpdateDeleteAPIView,
     SpotCategoryCheckAPIView
@@ -9,18 +10,14 @@ from travelhub.views.category_views import (
 urlpatterns = [
     # Public
     path("spot-categories/", SpotCategoryListAPIView.as_view()),
+    path("spot-categories/<str:slug>/", SpotCategoryDetailAPIView.as_view()),
 
     # Admin Utilities
-    path(
-        "admin/spot-categories/check-name/",
-        SpotCategoryCheckAPIView.as_view(),
-        name="spotcategory-check-name"
-    ),
+    path("admin/spot-categories/check-name/",
+         SpotCategoryCheckAPIView.as_view(), name="spotcategory-check-name"),
 
     # Admin
     path("admin/spot-categories/", SpotCategoryListCreateAPIView.as_view()),
-    path(
-        "admin/spot-categories/<str:slug>/",
-        SpotCategoryUpdateDeleteAPIView.as_view()
-    ),
+    path("admin/spot-categories/<str:slug>/",
+         SpotCategoryUpdateDeleteAPIView.as_view()),
 ]
