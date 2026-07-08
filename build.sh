ea37630f5857fef82b67e4608a66cd3b46088472
him8033 <himanshu8033@gmail.com>
2026-07-08 22:53:32 +0530

    update build.sh temp

--- build.sh
+++ build.sh
@@ -14,16 +14,16 @@ python manage.py collectstatic --noinput
 echo "🛠 Running migrations..."
 python manage.py migrate --noinput
 
-# echo "👤 Creating superuser..."
-# python manage.py create_admin || true
+echo "👤 Creating superuser..."
+python manage.py create_admin || true
 
-# echo "🌍 Importing data (only first time)..."
+echo "🌍 Importing data (only first time)..."
 
-# python manage.py import_countries || true
-# python manage.py import_states || true
-# python manage.py import_districts || true
-# python manage.py import_subdistricts || true
-# python manage.py import_villages || true
-# python manage.py import_pincodes || true
+python manage.py import_countries || true
+python manage.py import_states || true
+python manage.py import_districts || true
+python manage.py import_subdistricts || true
+python manage.py import_villages || true
+python manage.py import_pincodes || true
 
 echo "✅ Build completed!"
