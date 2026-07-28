54ceeba2b9492a4a08734296cd90fdb23138a320
him8033 <himanshu8033@gmail.com>
2026-07-28 20:30:23 +0530

    update ordering login in education section

--- portfoliohub/serializers/profile_education.py
+++ portfoliohub/serializers/profile_education.py
@@ -60,6 +60,24 @@ class ProfileEducationSerializer(serializers.ModelSerializer):
             user=request.user
         )
 
+        # ----------------------------------------
+        # Auto assign last position
+        # ----------------------------------------
+        if validated_data.get("position") is None:
+            last = (
+                ProfileEducation.objects.filter(
+                    profile_snapshot=snapshot
+                )
+                .order_by("-position")
+                .first()
+            )
+
+            validated_data["position"] = (
+                (last.position + 1)
+                if last and last.position is not None
+                else 0
+            )
+
         return ProfileEducation.objects.create(
             profile_snapshot=snapshot,
             **validated_data
